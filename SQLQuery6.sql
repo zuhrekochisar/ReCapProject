6ef9d9623fe8a1408b73fc1c40a3050d0a60f3e4
Zühre Koçhisar <zuhrekochisar@gmail.com>
2021-02-19 23:33:07 +0300

    Car Rental System

---
+++ SQLQuery6.sql
@@ -0,0 +1,21 @@
+﻿insert into cars (BrandId,ColorId,ModelYear,DailyPrice,Descriptions)
+values('1','2','2017','600','Kiralık BMW'),
+('1','2','2010','500','Kiralık BMW'),
+('2','3','2015','800','Kiralık Mercedes'),
+('3','4','2020','700','Kiralık Toyota'),
+('4','5','2010','300','Kiralık Honda'),
+('4','6','2010','300','Kiralık Honda'),
+('6','3','2010','300','Kiralık Hyundai')
+
+insert into cars Values ('6','5','2019','400','Kiralık Hyundai')
+insert into Cars values ('1','4','2014','300','Kiralık BMW')
+
+
+
+--update Cars set CarId=8
+
+--delete from Cars where Descriptions='Kiralık Hyundai'
+delete from Cars where CarId = 9
+
+
+
