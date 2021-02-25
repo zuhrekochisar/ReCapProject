536c204ebf757b926cac354feaaa6117b5639898
Zühre Koçhisar <zuhrekochisar@gmail.com>
2021-02-25 21:58:44 +0300

    Car Rental System

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -14,6 +14,11 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(u =>u.UserFirstName).MinimumLength(2);
             RuleFor(u => u.UserLastName).NotEmpty();
             RuleFor(u => u.UserLastName).MinimumLength(2);
+            RuleFor(u => u.UserEmail).NotEmpty();
+            RuleFor(u => u.UserEmail).EmailAddress().WithMessage("Geçerli bir e-posta adresi gereklidir.");
+            RuleFor(u => u.UserPassword).NotEmpty().WithMessage("Geçerli bir şifre giriniz.");
+
+
         }
     }
 }
